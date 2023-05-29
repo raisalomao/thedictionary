22652c8292757fd52f8b7b092e82c4868172b490
Raí Salomão <123793652+raisalomao@users.noreply.github.com>
2023-05-29 14:56:33 -0300

    Add files via upload

--- src/requisition.py
+++ src/requisition.py
@@ -2,13 +2,13 @@ import aiohttp
 import asyncio
 
 async def main():
-    palavras = ['amor', 'moça', 'passas']
+    palavras = ['amor', 'moça', 'venda']
 
     async with aiohttp.ClientSession(headers={'User-Agent': 'Mozilla/5.0'}) as get:
-        tasks = [get.get(url=f'https://dicionario-solomon.onrender.com/{palavra}') for palavra in palavras]
+        tasks = [get.get(url=f'https://dicionario-solomon.onrender.com/{palavra}?format=json') for palavra in palavras]
         responses = await asyncio.gather(*tasks)
         meanings = [await response.json() for response in responses]
         for meaning in meanings:
-            print(meaning['results'][0]['examples'][0]['sentences'])
+            print(meaning['results'][0]['examples'][0]['sentence'])
 
 asyncio.run(main())
